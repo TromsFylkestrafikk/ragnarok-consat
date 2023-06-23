ed48b1e7d0edcfa2eeeae54102d03c8c72ec0b99
Kåre Slettnes <kaare.slettnes@tffk.no>
2023-06-23 14:46:23 +0200

    Issue #1: Removed code now in RagnarokSink

--- src/Services/ConsatHistoric.php
+++ src/Services/ConsatHistoric.php
@@ -2,18 +2,16 @@
 
 namespace TromsFylkestrafikk\RagnarokConsat\Services;
 
+use Illuminate\Contracts\Filesystem\Filesystem;
 use TromsFylkestrafikk\RagnarokConsat\Models\Call;
 use TromsFylkestrafikk\RagnarokConsat\Models\CallDetail;
 use TromsFylkestrafikk\RagnarokConsat\Models\Destination;
-use TromsFylkestrafikk\RagnarokConsat\Models\RawFile;
 use TromsFylkestrafikk\RagnarokConsat\Models\PassengerCount;
 use TromsFylkestrafikk\RagnarokConsat\Models\PlannedJourney;
 use TromsFylkestrafikk\RagnarokConsat\Models\Stop;
 use TromsFylkestrafikk\RagnarokSink\Traits\LogPrintf;
-use Exception;
+use TromsFylkestrafikk\RagnarokSink\Services\RemoteFile;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Contracts\Filesystem\Filesystem;
 
 /**
  * Services around historic route and real time data from Consat.
@@ -23,6 +21,11 @@ class ConsatHistoric
     use LogPrintf;
 
     public const DATE_REGEX = "/^(?P<date>20\d{2}-\d{2}-\d{2})\.(?P<ext>7z|zip)$/";
+
+    /**
+     * @var RemoteFile
+     */
+    protected $remoteFile = null;
 
     protected $dataModels = [
         Call::class,
@@ -34,16 +37,6 @@ class ConsatHistoric
     ];
 
     /**
-     * @var Filesystem
-     */
-    protected $rDisk = null;
-
-    /**
-     * @var Filesystem
-     */
-    protected $lDisk = null;
-
-    /**
      * Timestamp of last re-connect to remote disk
      *
      * @var Carbon|null
@@ -53,67 +46,7 @@ class ConsatHistoric
     public function __construct()
     {
         $this->logPrintfInit('[ConsatService]: ');
-    }
-
-    /**
-     * Get zip file model for given date.
-     *
-     * @param string $date
-     *
-     * @return RawFile|null
-     */
-    public function getZipFile($date)
-    {
-        /** @var RawFile $file */
-        $file = RawFile::where('date', $date)->first();
-        if (!$file) {
-            $file = $this->createZipFile($date);
-        } elseif (!$this->getLocalDisk()->exists($file->name)) {
-            $file = $this->refreshZipFile($file);
-        }
-        return $file;
-    }
-
-    /**
-     * Check existence and checksum for given local file.
-     *
-     * @return bool
-     */
-    public function localChecksOut(RawFile $file)
-    {
-        $lDisk = $this->getLocalDisk();
-        return $lDisk->exists($file->name) && md5($lDisk->get($file->name)) === $file->checksum;
-    }
-
-    /**
-     * Compare local file with remote, download and update status.
-     *
-     * @param RawFile $file
-     *
-     * @return RawFile
-     */
-    public function refreshZipFile(RawFile $file)
-    {
-        $newContent = $this->getRemoteFileContent($file->name);
-        $existsLocal = $this->getLocalDisk()->exists($file->name);
-        if (!$newContent) {
-            // Server might be down. Not touching state of file unless the local
-            // file is missing.
-            if (!$existsLocal) {
-                throw new Exception("Missing both local and remote file. Consider 'consat:purge'");
-            }
-            return $file;
-        }
-        $newChecksum = md5($newContent);
-        if ($newChecksum !== $file->checksum || !$existsLocal) {
-            $this->getLocalDisk()->put($file->name, $newContent);
-            if ($newChecksum !== $file->checksum) {
-                $file->checksum = $newChecksum;
-                $file->import_status = 'updated';
-                $file->save();
-            }
-        }
-        return $file;
+        $this->remoteFile = new RemoteFile('Consat', $this->buildRemoteDisk());
     }
 
     /**
@@ -121,36 +54,12 @@ class ConsatHistoric
      *
      * @param string $date
      */
-    public function purgeDay($date)
+    public function purgeDay($date): void
     {
         foreach ($this->dataModels as $modelName) {
             $modelName::where('date', $date)->delete();
         }
-        $file = RawFile::whereDate('date', $date)->first();
-        if (!$file) {
-            return;
-        }
-        $file->import_status = 'new';
-        $file->save();
-    }
-
-    /**
-     * Purge the copied zip file for a given day
-     *
-     * @param string $date
-     */
-    public function purgeZipFile($date)
-    {
-        $this->purgeDay($date);
-        $file = RawFile::where('date', $date)->first();
-        if (!$file) {
-            return;
-        }
-        $lDisk = $this->getLocalDisk();
-        if ($lDisk->exists($file->name)) {
-            $lDisk->delete($file->name);
-        }
-        $file->delete();
+        $this->remoteFile->resetImportStatus($this->filenameFromDate($date));
     }
 
     /**
@@ -158,7 +67,7 @@ class ConsatHistoric
      *
      * @return $this
      */
-    public function cleanupAncient($months = 3)
+    public function cleanupAncient($months = 3): ConsatHistoric
     {
         $this->info("BEGIN cleanup of ancient data ...");
         // Stub.
@@ -171,40 +80,13 @@ class ConsatHistoric
     }
 
     /**
-     * @return Filesystem
-     */
-    public function getRemoteDisk()
-    {
-        if (!$this->rDisk) {
-            $this->reConnect();
-        } else {
-            $timeout = (new Carbon())->subSeconds(config('filesystems.consat_historic_remote.timeout'));
-            if ($this->lastConnect->isAfter($timeout)) {
-                $this->reConnect();
-            }
-        }
-        return $this->rDisk;
-    }
-
-    /**
-     * @return Filesystem
-     */
-    public function getLocalDisk()
-    {
-        if (!$this->lDisk) {
-            $this->lDisk = Storage::disk('consat_historic_local');
-        }
-        return $this->lDisk;
-    }
-
-    /**
      * List remote files.
      *
      * @return array
      */
-    public function listRemote()
+    public function listRemote(): array
     {
-        return $this->fileFilter($this->getRemoteDisk()->allFiles());
+        return $this->fileFilter($this->remoteFile->getRemoteDisk()->allFiles());
     }
 
     /**
@@ -212,9 +94,17 @@ class ConsatHistoric
      *
      * @return array
      */
-    public function listLocal()
+    public function listLocal(): array
     {
-        return $this->fileFilter($this->getLocalDisk()->allFiles());
+        return $this->fileFilter($this->remoteFile->getLocalDisk()->allFiles());
+    }
+
+    /**
+     * Build file name used for daily dump.
+     */
+    public function filenameFromDate(string $dateStr): string
+    {
+        return $dateStr . '.7z';
     }
 
     /**
@@ -234,85 +124,22 @@ class ConsatHistoric
         return $matches['date'];
     }
 
-    /**
-     * Create a new zip file model for given date.
-     *
-     * @return RawFile|null
-     */
-    protected function createZipFile($date)
+    protected function buildRemoteDisk(): Filesystem
     {
-        $filename = $date . ".7z";
-        $copied = $this->copyFile($filename);
-        if (!$copied) {
-            return null;
-        }
-
-        /** @var RawFile $file */
-        $file = RawFile::create([
-            'name' => $filename,
-            'checksum' => md5($this->getLocalDisk()->get($filename)),
-            'date' => $date,
-            'import_status' => 'new',
-            'import_msg' => null,
-        ]);
-        return $file;
-    }
-
-    /**
-     * @param RawFile $zipFile
-     *
-     * @return RawFile
-     */
-    protected function updateZipFile($zipFile)
-    {
-        $copyOk = $this->copyFile($zipFile->name);
-        if (!$copyOk) {
-            $zipFile->import_status = 'empty';
-            $zipFile->checksum = md5(random_bytes(256));
-        } else {
-            $zipFile->import_status = 'updated';
-            $zipFile->checksum = md5($this->getLocalDisk()->get($zipFile->name));
-        }
-        $zipFile->save();
-        return $zipFile;
-    }
-
-    /**
-     * Copies file from remote to local
-     *
-     * @param string $filename
-     *
-     * @return bool True if success
-     */
-    protected function copyFile($filename)
-    {
-        $content = $this->getRemoteFileContent($filename);
-        if (!$content) {
-            return false;
-        }
-        return $this->getLocalDisk()->put($filename, $content);
-    }
-
-    protected function getRemoteFileContent($filename)
-    {
-        $rDisk = $this->getRemoteDisk();
-        if (!$rDisk->exists($filename)) {
-            return null;
-        }
-        return $rDisk->get($filename);
+        return app('filesystem')->build(config('ragnarok_consat.remote_disk'));
     }
 
     /**
      * @return $this
      */
-    protected function reConnect()
+    protected function reConnect(): ConsatHistoric
     {
-        $this->rDisk = Storage::disk('consat_historic_remote');
+        $this->remoteFile->setRemoteDisk($this->buildRemoteDisk());
         $this->lastConnect = new Carbon();
         return $this;
     }
 
-    protected function fileFilter($filenames)
+    protected function fileFilter($filenames): array
     {
         return array_filter($filenames, function ($filename) {
             return preg_match(self::DATE_REGEX, $filename);
