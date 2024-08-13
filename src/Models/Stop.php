e6b7775c1602541adda4fefbeae46925a76d0b62
Kåre Slettnes <kaare.slettnes@tromsfylke.no>
2024-08-13 09:17:08 +0200

    Migration in place. non-working import code attempt

--- src/Models/Stop.php
+++ src/Models/Stop.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Stop extends Model
 {
     public $timestamps = false;
+    public $incrementing = false;
     protected $table = 'consat_stops';
+    protected $keyType = 'string';
 }
