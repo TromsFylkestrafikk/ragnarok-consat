16deeed0b2f3c4a6a04eada4d8e7a36acba00479
Kåre Slettnes <kaare.slettnes@tromsfylke.no>
2025-06-17 12:51:45 +0200

    Untested: Migration and model

---
+++ src/Models/PassengerCount.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ragnarok\Consat\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PassengerCount extends Model
+{
+    public $timestamps = false;
+    protected $table = 'consat_passenger_count';
+}
